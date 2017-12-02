fb974bb45201d75850fe9451b16cd7f02c83ee91
EC2 Default User <ec2-user@ip-172-31-35-142.us-east-2.compute.internal>
2017-12-02 02:17:34 +0000

    add test

--- SampleTest.php
+++ SampleTest.php
@@ -2,7 +2,7 @@
 require_once('./Sample.php');
 class SampleTest extends PHPUnit_Framework_TestCase
 {
-    public function testハローワールド()
+    public function testHelloworld()
     {
         $Sample = new Sample();
         $this->assertEquals($Sample->helloWorld(), 'hello world!');
